221edf7b1fdbd6099b5cd09e728dc5a25e17d85f
vvignesh501 <43393145+vvignesh501@users.noreply.github.com>
2025-08-26 18:12:57 -0400

    Time: 0 ms (100%), Space: 17.8 MB (50.9%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -5,8 +5,11 @@ class Solution:
         stack = []
 
         for ch in s:
+            # Append when opening bracket
             if ch in brackets:
                 stack.append(ch)
+            # when ch == closing bracket, 
+            # pop the stack and check if opening has a closing bracket
             else:
                 if not stack:
                     return False
@@ -15,4 +18,7 @@ class Solution:
                     return False
         return len(stack) == 0
 
+
+# Time = O(n)
+# Space = O(n)
         
