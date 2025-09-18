961d20ced55d7caf98f2d0e9f53f1f79ab27a4a2
vvignesh501 <43393145+vvignesh501@users.noreply.github.com>
2025-09-17 21:50:49 -0400

    Time: 1 ms (66.88%), Space: 18.5 MB (91.7%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.py
@@ -0,0 +1,22 @@
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+
+        n = len(nums)
+        xor = 0
+
+        # Create a xor array from 0,1,2,3
+        for i in range(n + 1):
+            xor = xor ^ i
+
+        # Now xor with original nums array.
+        # XORing basically cancells the duplicates i.e ones 
+        # the nums existing & keeps only the missing number
+        # thats how xor works
+
+        for num in nums:
+            xor = xor ^ num
+
+        return xor
+        
+# Time - O(n)
+# Space - O(1)
