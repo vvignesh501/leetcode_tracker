6f636fbe8c69f74dcededf85d522c976f7d0c755
vvignesh501 <43393145+vvignesh501@users.noreply.github.com>
2025-09-24 00:09:30 -0400

    Time: 142 ms (42.65%), Space: 19.3 MB (28.32%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.py
+++ 0621-task-scheduler/0621-task-scheduler.py
@@ -1,30 +1,37 @@
+from typing import List
+from collections import Counter, deque
+import heapq
+
 class Solution:
     def leastInterval(self, tasks: List[str], n: int) -> int:
-
-        # Neetcode - https://www.youtube.com/watch?v=s8p8ukTyA2I
         count = Counter(tasks)
         maxHeap = [-cnt for cnt in count.values()]
-        # Create a minHeap to get the max value first(use -ve so you get max)
-        # Create a queue to monitor whn the next A or B can be added back to minHeap
         heapq.heapify(maxHeap)
-        q = deque()
+
         time = 0
+        q = deque()  # stores pairs: (ready_time, count)
 
         while maxHeap or q:
-            
             time += 1
-            # Calculate heap = Reduce from A=3 to A=2 & append to q when next A can be added to maxHeap
+
             if maxHeap:
-                rem = 1 + heapq.heappop(maxHeap)
-                if rem:
-                    q.append((rem, time + n))
-        
-            # # Calculate queue = When time matches the task
-            if q and q[0][1] == time:
-                heapq.heappush(maxHeap, q.popleft()[0])
+                cnt = 1 + heapq.heappop(maxHeap)  # run the most frequent task
+                if cnt != 0:
+                    # push into cooldown queue: available again at time+n
+                    q.append((time + n, cnt))
+
+            if q and q[0][0] == time:  # task ready to be re-added
+                heapq.heappush(maxHeap, q.popleft()[1])
 
         return time
 
-# Time Complexity: O(tasks * log 26) → O(tasks)
+# Time Complexity: O(T log k) → since k ≤ 26, this is effectively O(T).
 
-# Space Complexity: O(26 + n) → O(1)
+# Space Complexity:
+
+# Counter: O(k)
+
+# Heap: O(k)
+
+# Queue: O(k)
+# → O(k) ≈ O(1) (bounded by 26).
