7e393cd3987ab84183fa3ac581c69642d547098a
vvignesh501 <43393145+vvignesh501@users.noreply.github.com>
2025-09-29 00:44:54 -0400

    Time: 0 ms (100%), Space: 17.9 MB (48.73%) - LeetHub

---
+++ 0063-unique-paths-ii/0063-unique-paths-ii.py
@@ -0,0 +1,17 @@
+class Solution:
+    def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
+
+        row = len(obstacleGrid) 
+        col = len(obstacleGrid[0])   
+        dp = [0] * col
+        dp[col - 1] = 1
+
+        # Bottom up approach with only one DP in memory needed.
+        # Trick here is you don't need 2 dimensions for dp in memory
+        for r in range(row - 1, -1, -1):
+            for c in range(col - 1, -1, -1):
+                if obstacleGrid[r][c]:
+                    dp[c] = 0
+                elif c + 1 < col:
+                    dp[c] = dp[c] + dp[c + 1]
+        return dp[0]
