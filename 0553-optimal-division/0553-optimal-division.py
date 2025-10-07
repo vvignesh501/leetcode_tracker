65876159b2ebdbe697a209469cd7d412beb1e369
vvignesh501 <43393145+vvignesh501@users.noreply.github.com>
2025-10-07 10:03:45 -0400

    Time: 0 ms (100%), Space: 17.9 MB (48.16%) - LeetHub

---
+++ 0553-optimal-division/0553-optimal-division.py
@@ -0,0 +1,11 @@
+class Solution:
+    def optimalDivision(self, nums: List[int]) -> str:
+
+        if len(nums) == 1:
+            return str(nums[0])
+        if len(nums) == 2:
+            return f"{nums[0]}/{nums[1]}"
+
+        numerator = str(nums[0])
+        denominator = '/'.join(map(str, nums[1:]))
+        return f"{numerator}/({denominator})"
