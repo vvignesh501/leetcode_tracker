3c0f244ac1fefd6b7fdf9c077a54e25a0aff9c3d
vvignesh501 <43393145+vvignesh501@users.noreply.github.com>
2025-10-07 12:15:21 -0400

    Time: 0 ms (100%), Space: 18 MB (34.29%) - LeetHub

---
+++ 0046-permutations/0046-permutations.py
@@ -0,0 +1,22 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        
+        # The trick is remembering the visited node everytime dfs is called
+        # Once you backtrack and remove the res, remove the visited too.
+
+        perms=[]
+        solution=[]
+
+        def backtrack():
+            if len(solution)==len(nums):
+                perms.append(solution.copy())
+                return 
+
+            for num in nums:
+                if num not in solution:
+                    solution.append(num)
+                    backtrack()
+                    solution.pop()
+                    
+        backtrack()
+        return perms
