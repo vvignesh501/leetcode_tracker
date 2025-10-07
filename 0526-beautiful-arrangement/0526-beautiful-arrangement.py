92704cf47f459876b49c91a8d27a4c2b49a47d11
vvignesh501 <43393145+vvignesh501@users.noreply.github.com>
2025-10-07 13:31:02 -0400

    Time: 944 ms (22.09%), Space: 17.6 MB (94.9%) - LeetHub

---
+++ 0526-beautiful-arrangement/0526-beautiful-arrangement.py
@@ -0,0 +1,18 @@
+class Solution:
+    def countArrangement(self, n: int) -> int:
+        
+        res = set()
+        def backtracking(pos, res):
+
+            if pos > n:
+                return 1
+            
+            count = 0
+            for i in range(1, n + 1):
+                if i not in res and (i % pos == 0 or pos % i == 0):
+                    res.add(i)
+                    count += backtracking(pos + 1, res)
+                    res.remove(i)
+            return count
+        
+        return backtracking(1, res)
