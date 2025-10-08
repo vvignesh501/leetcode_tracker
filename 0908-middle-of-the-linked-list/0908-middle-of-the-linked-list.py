bddf3adab322710947f82bbc9f8aaeccce747c9c
vvignesh501 <43393145+vvignesh501@users.noreply.github.com>
2025-10-08 18:44:11 -0400

    Time: 0 ms (100%), Space: 17.8 MB (48.57%) - LeetHub

--- 0908-middle-of-the-linked-list/0908-middle-of-the-linked-list.py
+++ 0908-middle-of-the-linked-list/0908-middle-of-the-linked-list.py
@@ -7,6 +7,9 @@ class Solution:
     def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
         tail = head
 
+        # tail.next = the end of the array. Tht's why tail.next check is needed.
+        # So that the program ends and when it's the end.
+        
         while tail and tail.next:
             head = head.next
             tail = tail.next.next
