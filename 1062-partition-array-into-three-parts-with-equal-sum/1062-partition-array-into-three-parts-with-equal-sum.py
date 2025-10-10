6388e880e0e94dc79bbacac8e7d6e734b92cb2f1
vvignesh501 <43393145+vvignesh501@users.noreply.github.com>
2025-10-10 17:15:09 -0400

    Time: 4 ms (71.78%), Space: 23.2 MB (57.29%) - LeetHub

---
+++ 1062-partition-array-into-three-parts-with-equal-sum/1062-partition-array-into-three-parts-with-equal-sum.py
@@ -0,0 +1,22 @@
+class Solution:
+    def canThreePartsEqualSum(self, arr: List[int]) -> bool:
+        
+        total = sum(arr)
+        avg = total // 3
+        count = 0
+        curr_sum = 0
+
+        if total % 3 != 0:
+            return False
+
+        for i in range(len(arr)):
+            curr_sum += arr[i]
+
+            if curr_sum == avg:
+                count += 1
+                curr_sum = 0
+            
+            if count == 3:
+                return True
+        
+        return False
