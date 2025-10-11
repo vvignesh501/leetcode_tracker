3572084f789efad0faec380a2933b16d664ad0d1
vvignesh501 <43393145+vvignesh501@users.noreply.github.com>
2025-10-11 18:33:39 -0400

    Time: 23 ms (70.07%), Space: 29.3 MB (15.84%) - LeetHub

--- 0187-repeated-dna-sequences/0187-repeated-dna-sequences.py
+++ 0187-repeated-dna-sequences/0187-repeated-dna-sequences.py
@@ -1,18 +1,32 @@
 class Solution:
     def findRepeatedDnaSequences(self, s: str) -> List[str]:
 
+#         k = 10
+#         dna = {}
+#         res = []
+
+#         for i in range(len(s)- k + 1):
+#             substring = s[i:i + k]
+#             dna[substring] = 1 + dna.get(substring, 0)
+        
+#         for k, v in dna.items():
+#             if v > 1:
+#                 res.append(k)
+#         return res
+
+# # Time - O(n)
+# # Space - O(n)
+
         k = 10
-        dna = {}
-        res = []
+        repeated = set()
+        seen = set()
 
         for i in range(len(s)- k + 1):
             substring = s[i:i + k]
-            dna[substring] = 1 + dna.get(substring, 0)
-        
-        for k, v in dna.items():
-            if v > 1:
-                res.append(k)
-        return res
 
-# Time - O(n)
-# Space - O(n)
+            if substring in seen:
+                repeated.add(substring)
+            seen.add(substring)
+        
+        return list(repeated)
+            
