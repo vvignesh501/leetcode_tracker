6a9d61be28ce859e766a9106e521708ed22a580d
vvignesh501 <43393145+vvignesh501@users.noreply.github.com>
2025-10-11 17:02:12 -0400

    Time: 116 ms (72.31%), Space: 27.8 MB (52.01%) - LeetHub

---
+++ 2553-total-cost-to-hire-k-workers/2553-total-cost-to-hire-k-workers.py
@@ -0,0 +1,35 @@
+import heapq
+
+class Solution:
+    def totalCost(self, costs: List[int], k: int, candidates: int) -> int:
+        n = len(costs)
+        total = 0
+        
+        # Two pointers
+        l, r = 0, n - 1
+        
+        left_heap, right_heap = [], []
+        
+        # Fill initial heaps
+        for _ in range(candidates):
+            if l <= r:
+                heapq.heappush(left_heap, costs[l])
+                l += 1
+            if l <= r:
+                heapq.heappush(right_heap, costs[r])
+                r -= 1
+        
+        for _ in range(k):
+            # Choose cheaper of two sides
+            if not right_heap or (left_heap and left_heap[0] <= right_heap[0]):
+                total += heapq.heappop(left_heap)
+                if l <= r:
+                    heapq.heappush(left_heap, costs[l])
+                    l += 1
+            else:
+                total += heapq.heappop(right_heap)
+                if l <= r:
+                    heapq.heappush(right_heap, costs[r])
+                    r -= 1
+        
+        return total
