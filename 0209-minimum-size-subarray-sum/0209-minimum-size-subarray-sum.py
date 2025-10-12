9330c7fd71d5e57654c4446d9ab1c26d822cd435
vvignesh501 <43393145+vvignesh501@users.noreply.github.com>
2025-10-12 14:36:49 -0400

    Time: 7 ms (91.67%), Space: 28.4 MB (37.14%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.py
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.py
@@ -8,6 +8,7 @@ class Solution:
         for r in range(len(nums)):
             total += nums[r]
             
+            # shrink the left window until total >= target
             while total >= target:
                 min_len = min(min_len, (r - l + 1))
                 total -= nums[l]
@@ -16,6 +17,12 @@ class Solution:
         return min_len if min_len < float("inf") else 0
 
             
+# Time Complexity: O(n)
 
+# Each element is added once and removed once from current_sum.
 
-        
+# So the total number of operations is proportional to 2n.
+
+# Space Complexity: O(1)
+
+# We only use a few variables (left, current_sum, min_len) regardless of the array size. 
