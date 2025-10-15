5b0f54dd048004c08f92e3eac12fbdce9bce7c51
vvignesh501 <43393145+vvignesh501@users.noreply.github.com>
2025-10-15 15:07:30 -0400

    Time: 0 ms (100%), Space: 17.6 MB (82.85%) - LeetHub

---
+++ 1115-valid-boomerang/1115-valid-boomerang.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isBoomerang(self, points: List[List[int]]) -> bool:
+
+        # Area of a triangle = 1/2 × abs(x1(y2−y3)+x2(y3−y1)+x3(y1−y2))
+        x1, y1 = points[0]
+        x2, y2 = points[1]
+        x3, y3 = points[2]
+
+        area = abs(x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2))/2
+        return area != 0
+        
