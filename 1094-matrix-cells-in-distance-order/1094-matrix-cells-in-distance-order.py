f736161703b67d963d67c26d9411c48d8453ef1b
vvignesh501 <43393145+vvignesh501@users.noreply.github.com>
2025-10-14 21:39:42 -0400

    Time: 11 ms (82.04%), Space: 19.2 MB (85.56%) - LeetHub

---
+++ 1094-matrix-cells-in-distance-order/1094-matrix-cells-in-distance-order.py
@@ -0,0 +1,11 @@
+class Solution:
+    def allCellsDistOrder(self, rows: int, cols: int, rCenter: int, cCenter: int) -> List[List[int]]:
+        answer = []
+        
+        for i in range(rows):
+            for j in range(cols):
+                answer.append([i,j])
+        
+        answer.sort(key=lambda x: abs(x[0]- rCenter) + abs(x[1] - cCenter))
+        
+        return answer
