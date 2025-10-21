68c5fac851d97a99ee668252a5931723df51ccab
vvignesh501 <43393145+vvignesh501@users.noreply.github.com>
2025-10-21 14:26:25 -0400

    Time: 119 ms (77.31%), Space: 78 MB (72.47%) - LeetHub

---
+++ 0146-lru-cache/0146-lru-cache.py
@@ -0,0 +1,26 @@
+from collections import OrderedDict
+
+class LRUCache:
+    def __init__(self, capacity: int):
+        self.cache = OrderedDict()
+        self.capacity = capacity
+
+    def get(self, key: int) -> int:
+        if key not in self.cache:
+            return -1
+        # Move accessed key to end (most recent)
+        self.cache.move_to_end(key)
+        return self.cache[key]
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            # Update and move to end
+            self.cache.move_to_end(key)
+        self.cache[key] = value
+        if len(self.cache) > self.capacity:
+            # Pop least recently used (first item)
+            self.cache.popitem(last=False)
+
+# Time - get: O(n) (because of remove on order list)
+
+# Space - put: O(n) (same reason)
