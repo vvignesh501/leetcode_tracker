0a099e844ebfe812e59d3859c4cbfc19a3975203
vvignesh501 <43393145+vvignesh501@users.noreply.github.com>
2025-10-24 17:17:41 -0400

    Time: 0 ms (100%), Space: 18 MB (26%) - LeetHub

---
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def merge(self, nums1, m, nums2, n):
+
+        i = m - 1
+        j = n - 1
+        k = m + n - 1
+        
+        # Trick here is to remember that nums1 have a total of k length instead of m mentioned in the question.
+        # Make sure you create a new array with k length. Do not try to insert in the same n length
+        # Which ever is highest value from traversing reverse should be under nums1 filled from reverse.
+        
+        while j >= 0:
+            if i >= 0 and nums1[i] > nums2[j]:
+                nums1[k] = nums1[i]
+                i -= 1
+            else:
+                nums1[k] = nums2[j]
+                j -= 1
+            k -= 1
+
+# Time complexity: O(m+n)
+# Space complexity: O(1)
