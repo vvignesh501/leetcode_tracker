a1144b929781786564c8cf217dcf92c7efd9c806
vvignesh501 <43393145+vvignesh501@users.noreply.github.com>
2025-10-24 18:00:48 -0400

    Time: 0 ms (100%), Space: 18.9 MB (34.56%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,22 +1,33 @@
+# class Solution:
+#     def missingNumber(self, nums: List[int]) -> int:
+
+#         n = len(nums)
+#         xor = 0
+
+#         # Create a xor array from 0,1,2,3
+#         for i in range(n + 1):
+#             xor = xor ^ i
+
+#         # Now xor with original nums array.
+#         # XORing basically cancells the duplicates i.e ones 
+#         # the nums existing & keeps only the missing number
+#         # thats how xor works
+
+#         for num in nums:
+#             xor = xor ^ num
+
+#         return xor
+        
+# Time - O(n)
+# Space - O(1)
+
+
+# Formula - Missing number from a series of consecutive integers (1 to N) 
+# Formula: (Missing Number=(N(N+1)}\2) - Sum of the given numbers
+
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
 
         n = len(nums)
-        xor = 0
-
-        # Create a xor array from 0,1,2,3
-        for i in range(n + 1):
-            xor = xor ^ i
-
-        # Now xor with original nums array.
-        # XORing basically cancells the duplicates i.e ones 
-        # the nums existing & keeps only the missing number
-        # thats how xor works
-
-        for num in nums:
-            xor = xor ^ num
-
-        return xor
-        
-# Time - O(n)
-# Space - O(1)
+        missing = (n * (n + 1)//2) - sum(nums)
+        return missing
