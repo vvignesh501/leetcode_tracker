5f5c16420eb2277f9261622721f06f56022a19e6
vvignesh501 <43393145+vvignesh501@users.noreply.github.com>
2025-11-11 18:56:50 -0500

    Time: 271 ms (64.51%), Space: 17.8 MB (83.29%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,23 +1,19 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        
-        longestSubstring = ""
-        # Do the palindrome logic
-        def palindrome(l, r):
+
+        longest_substring = ""
+        def expand(l, r):
             while l >= 0 and r < len(s) and s[l] == s[r]:
                 l -= 1
                 r += 1
-            return s[l + 1:r]
-            # Make sure you return the substring
+            return s[l+1:r]
         
-        # Call the palindrome function for odd and even
         for i in range(len(s)):
-            odd = palindrome(i, i)
-            even = palindrome(i, i + 1)
-
-            longestSubstring = max(longestSubstring, odd, even, key=len)
+            odd = expand(i, i)
+            even = expand(i, i + 1)
+            longest_substring = max(longest_substring, odd, even, key=len)
         
-        return longestSubstring
+        return longest_substring
 
 # Time = O(n**2)
 # Space = O(1)
