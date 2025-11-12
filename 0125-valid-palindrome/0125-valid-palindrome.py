5700bd8e4c3b6f8378d5956a62cb6b1dabdee75f
vvignesh501 <43393145+vvignesh501@users.noreply.github.com>
2025-11-12 17:14:38 -0500

    Time: 12 ms (17.81%), Space: 23.1 MB (19.7%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -3,4 +3,7 @@ class Solution:
 
         cleaned_s = "".join(c.lower() for c in s if c.isalnum())
         return cleaned_s == cleaned_s[::-1]
-        
+
+# [::--1] doesn't change the original string it creates a copy in another list
+# Time - O(n)
+# Space - O(n) (for cleaned string
