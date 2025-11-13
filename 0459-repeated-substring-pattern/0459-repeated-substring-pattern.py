9289773d910efbd82b74e8a3c577256b5222e2b4
vvignesh501 <43393145+vvignesh501@users.noreply.github.com>
2025-11-13 17:19:39 -0500

    Time: 0 ms (100%), Space: 17.9 MB (57.32%) - LeetHub

--- 0459-repeated-substring-pattern/0459-repeated-substring-pattern.py
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.py
@@ -1,4 +1,12 @@
 class Solution:
     def repeatedSubstringPattern(self, s: str) -> bool:
         return s in (s + s)[1:-1]
-        
+
+
+#  Time → O(n)
+
+# Because s in (s+s)[1:-1] runs a substring search in a string of size 2n.
+
+# Space → O(n)
+
+# Because s+s allocates a new string of length 2n.
