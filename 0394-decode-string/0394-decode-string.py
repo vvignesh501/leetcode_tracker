25e90a2e34bbbbada4838a67e086801a5d9253d4
vvignesh501 <43393145+vvignesh501@users.noreply.github.com>
2025-11-13 16:51:23 -0500

    Time: 0 ms (100%), Space: 18 MB (12.19%) - LeetHub

---
+++ 0394-decode-string/0394-decode-string.py
@@ -0,0 +1,32 @@
+class Solution:
+    def decodeString(self, s: str) -> str:
+
+        stack = []
+        curr_str = ""
+        curr_num = 0 
+
+        for ch in s:
+
+            # Case 1 - int can be double digits
+            if ch.isdigit():
+                curr_num = curr_num * 10 + int(ch)
+
+            # Case - append stack as [char, k]
+            elif ch == '[':
+                stack.append((curr_str, curr_num))
+                # reset back curr_str, curr_num
+                curr_str = ""
+                curr_num = 0
+
+            # Case - when ] multiply the curr_str with curr_num
+            elif ch == ']':
+                prev_str, digit = stack.pop()
+                curr_str = prev_str + digit * curr_str
+            
+            # Case4 - when its not a digit or [ or ]
+            else:
+                curr_str += ch
+        
+        return curr_str
+                
+        
