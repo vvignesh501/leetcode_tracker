490887ebedd331cf8265cc2ba15cde32c284c295
vvignesh501 <43393145+vvignesh501@users.noreply.github.com>
2025-11-14 20:12:39 -0500

    Time: 7 ms (44.62%), Space: 25.7 MB (21.13%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -7,26 +7,26 @@ class Solution:
         #Solution 1 - Optimal  
         # When k > n we modulo to find the no of rotations needed
         # After k = n, the no of rotations are same
-        # n = len(nums)
-        # k = k % n
+        n = len(nums)
+        k = k % n
 
-        # def swap(l, r):
-        #     while l <= r:
-        #         nums[l], nums[r] = nums[r], nums[l]
-        #         l += 1
-        #         r -= 1
+        def swap(l, r):
+            while l <= r:
+                nums[l], nums[r] = nums[r], nums[l]
+                l += 1
+                r -= 1
 
-        # swap(0, n - 1)
-        # swap(0, k - 1)
-        # swap(k, n-1)
+        swap(0, n - 1)
+        swap(0, k - 1)
+        swap(k, n-1)
 
 # Time - O(n)
 # Space - O(1)
 
         # Solution 2 
-        k = k % len(nums)
-        if k != 0:
-            nums[:k], nums[k:] = nums[-k:], nums[:-k]
+        # k = k % len(nums)
+        # if k != 0:
+        #     nums[:k], nums[k:] = nums[-k:], nums[:-k]
 # Time - O(n)
 # Space - O(n) saves k values in memory while swapping
 
